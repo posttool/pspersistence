9a58d02ac2fddb293b31a0fb1526603cfa90ce77
topher <topher@241bf0be-db3f-0410-a781-e906b5fc445c>
2008-08-28 00:45:18 +0000

    git-svn-id: http://posttool.com/svn/posttool/PSPersistenceV1.0@783 241bf0be-db3f-0410-a781-e906b5fc445c

--- src/com/pagesociety/bdb/index/iterator/FREETEXTCONTAINSANYIndexIterator.java
+++ src/com/pagesociety/bdb/index/iterator/FREETEXTCONTAINSANYIndexIterator.java
@@ -1,84 +1,26 @@
 package com.pagesociety.bdb.index.iterator;
 
 
-import com.pagesociety.bdb.BDBSecondaryIndex;
+import java.util.Map;
+
 import com.sleepycat.bind.tuple.LongBinding;
 import com.sleepycat.db.DatabaseEntry;
 import com.sleepycat.db.DatabaseException;
 
 
 @SuppressWarnings("unchecked")
-public class FREETEXTCONTAINSANYIndexIterator extends SetIndexIterator 
+public class FREETEXTCONTAINSANYIndexIterator extends SETCONTAINSANYIndexIterator
 {
-	// both pointers to the same iterator. just different views.
-	protected RespositionableIndexIterator r_iter;
-	protected PredicateIndexIterator iter;
+
 	
 	public void open(IterableIndex index,Object... user_list_of_db_entries) throws DatabaseException
 	{
 		super.open(index,user_list_of_db_entries);
-		prepare_iterator();
-		iter.open(index,keys.get(current_key));		
-		advance_to_next();
+
 	}
 		
-	private void prepare_iterator()
-	{
-		if(globbing)
-		{
-			iter =  new STARTSWITHDATAIndexIterator();
-			r_iter = (RespositionableIndexIterator)iter;
-		}
-		else
-		{
-			iter = new EQIndexIterator();
-			r_iter = (RespositionableIndexIterator)iter;
-		}
-	}
-	
-	public void resume(BDBSecondaryIndex index,Object token) throws DatabaseException
-	{
-		super.resume(index,token);
-		prepare_iterator();
-		iter.resume(index,keys.get(current_key),data);
-		advance_to_next();
-
-	}
-	
-	public DatabaseEntry currentKey()
-	{		
-		return iter.key;
-	}
-	public DatabaseEntry currentData()
-	{
-		iter.data.setSize(8);
-		return iter.data;
-	}
-
-	public boolean isValid()
-	{
-		return iter.isValid();
-	}
-	
-	public boolean isDone()
-	{
-		return iter.isDone();
-	}
-	
-	public void next() throws DatabaseException
-	{
-		iter.next();		
-		advance_to_next();
-	}
-
-	public void close() throws DatabaseException
-	{
-		iter.close();	
-	}
-
 	protected void advance_to_next() throws DatabaseException
 	{
-
 		long l;
 		while(iter.isValid())
 		{
@@ -115,5 +57,22 @@ public class FREETEXTCONTAINSANYIndexIterator extends SetIndexIterator
 	/*!!!remember that currentData() returns truncated row so it is just the id 
 	 * and not id:pos
 	 */ 
-	 
+
+
+	public DatabaseEntry currentData()
+	{
+		iter.data.setSize(8);
+		return iter.data;
+	}
+	
+	public Map<String,Object> currentMetaData()
+	{
+		return null;
+	}
+	
+	public Map<String,Object> resultMetaData()
+	{
+		return null;
+	}
+
 }

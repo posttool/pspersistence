2d9e28efc03632dcb22e1df0c266d209cc1c1cc1
topher <topher@241bf0be-db3f-0410-a781-e906b5fc445c>
2009-03-31 19:08:16 +0000

    git-svn-id: http://posttool.com/svn/posttool/PSPersistenceV1.0@2039 241bf0be-db3f-0410-a781-e906b5fc445c

--- src/com/pagesociety/bdb/binding/EntityDefinitionBinding.java
+++ src/com/pagesociety/bdb/binding/EntityDefinitionBinding.java
@@ -33,22 +33,7 @@ public class EntityDefinitionBinding extends TupleBinding
 		for (int i = 0; i < fields.size(); i++)
 		{
 			FieldDefinition f = fields.get(i);
-			to.writeString(f.getName());
-			to.writeInt(f.getType());
-			if(f.getBaseType() == Types.TYPE_REFERENCE)
-				write_default_reference_value(f,f.getDefaultValue(),to);
-			else
-			{
-				try{
-				FieldBinding.writeValueToTuple(f, f.getDefaultValue(),to);
-				}catch(DatabaseException dbe){};/*we can ignore this since it will never happen as we are never saving reference types here */
-			}
-			if (f.getBaseType() == Types.TYPE_REFERENCE)
-				to.writeString(f.getReferenceType());
-		
-			to.writeBoolean(f.isRequired());
-			to.writeBoolean(f.isCascadeOnDelete());
-			to.writeString(f.getComment());
+			write_field_definition(f, to);
 		}
 
 	}
@@ -61,20 +46,7 @@ public class EntityDefinitionBinding extends TupleBinding
 		int size = ti.readInt();
 		for (int i = 0; i < size; i++)
 		{
-			FieldDefinition f = new FieldDefinition(ti.readString(), ti.readInt(),null);
-			Object default_value = null;
-			if (f.getBaseType() == Types.TYPE_REFERENCE)
-				default_value = read_default_reference_value(f,ti);
-			else
-				default_value = FieldBinding.readValueFromTuple(ti, f);
-			
-			f.setDefaultValue(default_value);
-			if (f.getBaseType() == Types.TYPE_REFERENCE)
-				f.setReferenceType(ti.readString());
-			
-			f.setIsRequired(ti.readBoolean());
-			f.setIsCascadeOnDelete(ti.readBoolean());
-			f.setComment(ti.readString());
+			FieldDefinition f = read_field_definition(ti);
 			entity_def.addField(f);
 		}
 
@@ -91,11 +63,50 @@ public class EntityDefinitionBinding extends TupleBinding
 
 	
 	//HELPERS//
+
+	public static void write_field_definition(FieldDefinition f,TupleOutput to)
+	{
+		to.writeString(f.getName());
+		to.writeInt(f.getType());
+		if(f.getBaseType() == Types.TYPE_REFERENCE)
+			write_default_reference_value(f,f.getDefaultValue(),to);
+		else
+		{
+			try{
+			FieldBinding.writeValueToTuple(f, f.getDefaultValue(),to);
+			}catch(DatabaseException dbe){};/*we can ignore this since it will never happen as we are never saving reference types here */
+		}
+		if (f.getBaseType() == Types.TYPE_REFERENCE)
+			to.writeString(f.getReferenceType());
+	
+		to.writeBoolean(f.isRequired());
+		to.writeBoolean(f.isCascadeOnDelete());
+		to.writeString(f.getComment());
+	}
+	
+	public static FieldDefinition read_field_definition(TupleInput ti)
+	{
+		FieldDefinition f = new FieldDefinition(ti.readString(), ti.readInt(),null);
+		Object default_value = null;
+		if (f.getBaseType() == Types.TYPE_REFERENCE)
+			default_value = read_default_reference_value(f,ti);
+		else
+			default_value = FieldBinding.readValueFromTuple(ti, f);
+		
+		f.setDefaultValue(default_value);
+		if (f.getBaseType() == Types.TYPE_REFERENCE)
+			f.setReferenceType(ti.readString());
+		
+		f.setIsRequired(ti.readBoolean());
+		f.setIsCascadeOnDelete(ti.readBoolean());
+		f.setComment(ti.readString());
+		return f;
+	}
 	
 	//the idea with this whole thing....we want to preserve the -1 id of the default
 	//value so we now to create a new one on insert. this seems like useful behavior.
 	//you can give every user a default collection automagically for instance	
-	private void write_default_reference_value(FieldDefinition f,Object value,TupleOutput to)
+	public static  void write_default_reference_value(FieldDefinition f,Object value,TupleOutput to)
 	{
 		if(f.isArray())
 			write_default_reference_array_value(value,to);
@@ -103,7 +114,7 @@ public class EntityDefinitionBinding extends TupleBinding
 			write_default_reference_value(value,to);
 	}
 	
-	private void write_default_reference_value(Object val,TupleOutput to)
+	public static void write_default_reference_value(Object val,TupleOutput to)
 	{
 
 		Entity e = (Entity)val;
@@ -117,7 +128,7 @@ public class EntityDefinitionBinding extends TupleBinding
 		to.writeLong(e.getId());			
 	}
 	
-	private void write_default_reference_array_value(Object val,TupleOutput to)
+	public static void write_default_reference_array_value(Object val,TupleOutput to)
 	{
 		List<Entity> ee = (List<Entity>)val;
 		if(ee == null)
@@ -137,7 +148,7 @@ public class EntityDefinitionBinding extends TupleBinding
 	//the idea with this whole thing....we want to preserve the -1 id of the default
 	//value so we now to create a new one on insert. this seems like useful behavior.
 	//you can give every user a default collection automagically for instance	
-	private Object read_default_reference_value(FieldDefinition f,TupleInput ti)
+	public static Object read_default_reference_value(FieldDefinition f,TupleInput ti)
 	{
 		if(f.isArray())
 			return read_default_reference_array_value(ti);
@@ -145,7 +156,7 @@ public class EntityDefinitionBinding extends TupleBinding
 			return read_default_reference_value(ti);
 	}
 	
-	private Object read_default_reference_value(TupleInput ti)
+	public static Object read_default_reference_value(TupleInput ti)
 	{
 		int null_flag = ti.readFast();
 		if(null_flag == FieldBinding.NULL_FLAG_VAL_NULL)
@@ -161,7 +172,7 @@ public class EntityDefinitionBinding extends TupleBinding
 		}
 	}
 	
-	private Object read_default_reference_array_value(TupleInput ti)
+	public static Object read_default_reference_array_value(TupleInput ti)
 	{
 
 		int null_flag = ti.readFast();
--- src/com/pagesociety/bdb/binding/EntitySecondaryIndexBinding.java
+++ src/com/pagesociety/bdb/binding/EntitySecondaryIndexBinding.java
@@ -5,20 +5,14 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+
 
 import org.apache.log4j.Logger;
 
-import com.pagesociety.bdb.BDBStore;
-import com.pagesociety.bdb.index.EntityIndexDefinition;
-import com.pagesociety.persistence.EntityDefinition;
 import com.pagesociety.persistence.EntityIndex;
 import com.pagesociety.persistence.FieldDefinition;
-import com.pagesociety.persistence.PersistenceException;
 import com.sleepycat.bind.tuple.TupleInput;
 import com.sleepycat.bind.tuple.TupleOutput;
 import com.sleepycat.db.DatabaseEntry;
@@ -41,13 +35,12 @@ public class EntitySecondaryIndexBinding
 		to.writeInt(s);
 		for(int i = 0;i < s;i++ )
 		{
+			FieldDefinition f = fields.get(i);
 			logger.debug("WRITING FIELD INTO INDEX INSTANCE "+entity_index.getName()+" FIELD NAME IS "+fields.get(i).getName());
-			to.writeString(fields.get(i).getName());
-			to.writeInt(fields.get(i).getType());
-			to.writeString(fields.get(i).getReferenceType());
+			EntityDefinitionBinding.write_field_definition(f, to);
 		}
 		
-        // Serialize to a byte array
+        // Serialize attributes to a byte array
         ByteArrayOutputStream bos = new ByteArrayOutputStream() ;
         ObjectOutputStream out;
 		try {
@@ -59,17 +52,10 @@ public class EntitySecondaryIndexBinding
 			throw new DatabaseException("FAILED SERIALIZING "+entity_index.getName()+". BAD BAD.");
 		}
 
-		//this is here to be backwards compatible with before we were
-		//dealing with attributes as serialized objects...num_attributes//
-		int num_attributes = entity_index.getAttributes().keySet().size();
-		//System.out.println("NUM ATTRIBUTES FOR "+entity_index.getName()+" IS "+num_attributes);
-		to.writeInt(num_attributes);
-		if(num_attributes != 0)
-		{
-			// Get the bytes of the serialized object
-			to.writeInt(bos.size());
-			to.writeFast(bos.toByteArray());
-		}
+		// Get the bytes of the serialized object
+		to.writeInt(bos.size());
+		to.writeFast(bos.toByteArray());
+
 		return new DatabaseEntry(to.toByteArray());
 	}
 
@@ -80,7 +66,7 @@ public class EntitySecondaryIndexBinding
 		String index_name 		= ti.readString();
 		int index_type 			= ti.readInt();
 		String entity_name 		= ti.readString();
-		//String field_name = ti.readString();
+
 	
 		EntityIndex esi = new EntityIndex(index_name, index_type);
 		esi.setEntity(entity_name);	
@@ -89,29 +75,27 @@ public class EntitySecondaryIndexBinding
 		int size = ti.readInt();
 		for(int i=0;i<size;i++)
 		{
-			FieldDefinition f = new FieldDefinition(ti.readString(),ti.readInt(),ti.readString());
+			FieldDefinition f = EntityDefinitionBinding.read_field_definition(ti);
 			esi.addField(f);//def.getField(ti.readString()));			
 		}
 		/*decode runtime attributes*/
-		int num_attributes = ti.readInt();
-		if(num_attributes != 0)
-		{	
-		    // Deserialize from a byte array
-			int serialized_size = ti.readInt();
-			byte[] buf = new byte[serialized_size];
-			ti.readFast(buf,0,serialized_size);
-			ObjectInputStream in;
-			Map<String,Object> index_attributes; 
-			try {
-				in = new ObjectInputStream(new ByteArrayInputStream(buf));
-				index_attributes = (Map<String,Object>) in.readObject();
-				in.close();	
-			} catch (Exception e) {
-				e.printStackTrace();
-				throw new DatabaseException("BARF IN DESERIALIZING INDEX ATTRIBUTES");
-			}		
-			esi.setAttributes(index_attributes);
-		}
+	
+	    // Deserialize from a byte array
+		int serialized_size = ti.readInt();
+		byte[] buf = new byte[serialized_size];
+		ti.readFast(buf,0,serialized_size);
+		ObjectInputStream in;
+		Map<String,Object> index_attributes; 
+		try {
+			in = new ObjectInputStream(new ByteArrayInputStream(buf));
+			index_attributes = (Map<String,Object>) in.readObject();
+			in.close();	
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new DatabaseException("BARF IN DESERIALIZING INDEX ATTRIBUTES");
+		}		
+		esi.setAttributes(index_attributes);
+
 		return esi;
 	}
 }
